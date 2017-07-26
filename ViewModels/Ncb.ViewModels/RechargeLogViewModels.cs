36cc6e7ebc383070ec261fc6debda4a7d87d8dea
lmxin <birdfishs@163.com>
2017-07-26 18:35:06 +0800

    调整解决方案结构

---
+++ ViewModels/Ncb.ViewModels/RechargeLogViewModels.cs
@@ -0,0 +1,38 @@
+﻿using Ncb.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ncb.AdminViewModels
+{
+    public class RechargeLogQueryViewModel : DeviceModel
+    {
+        public string QueryText { get; set; }
+        [Display(Name ="充值日期")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "到")]
+        public DateTime EndDate { get; set; }
+    }
+
+    public class RechargeLogQueryResultViewModel
+    {
+        [DisplayName("Mac")]
+        public string DeviceID { get; set; }
+        [DisplayName("名称")]
+        public string UserName { get; set; }
+        [DisplayName("手机号码")]
+        public string PhoneNumber { get; set; }
+        [DisplayName("充值金额")]
+        public decimal Amount { get; set; }
+        [DisplayName("充值日期")]
+        public DateTime RechargeDate { get; set; }
+        [DisplayName("到期时间")]
+        public string ExpiryDate { get; set; }
+        [DisplayName("操作人")]
+        public string Operator { get; set; }
+    }
+}
